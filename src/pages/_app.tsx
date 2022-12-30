5ac928120fb631bea3c4d069bcd401b2491f2888
harshith kalki <harshithkalki@gmail.com>
2022-12-30 20:25:41 +0530

    nav and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,79 @@
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
-import { trpc } from '../utils/trpc';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  AppShell,
+  Header,
+  MantineProvider,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+import { Provider } from "react-redux";
+import store from "../store";
+import { trpc } from "../utils/trpc";
+import NavbarSimple from "../components/Navbar";
+import { useMediaQuery } from "@mantine/hooks";
+import { useState } from "react";
 
 function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
+  const matches = useMediaQuery("(max-width: 800px)");
 
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>HashindCrm</title>
         <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width'
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: 'light',
+          colorScheme: "dark",
         }}
       >
-        <Component {...pageProps} />
+        <AppShell
+          padding="md"
+          navbar={<NavbarSimple hide={!opened} />}
+          header={
+            matches ? (
+              <Header p="md" height={50}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    height: "100%",
+                  }}
+                >
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    color={theme.colors.gray[6]}
+                    mr="xl"
+                  />
+                </div>
+              </Header>
+            ) : undefined
+          }
+          styles={(theme) => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          })}
+        >
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </AppShell>
       </MantineProvider>
     </>
   );
