1867882e7303aafac436bf6b58b967b82dd6c533
Abhiram <anukoolaabhiram@outlook.com>
2023-01-06 18:02:19 +0530

    removed unnecessary code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
-import styles from "./index.module.css";
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { trpc } from "../utils/trpc";
+import styles from './index.module.css';
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
   return <>hello</>;
 };
 
