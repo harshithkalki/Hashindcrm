088fd195c29dd2d3e2338a7462f57f43a42e8bff
harshith kalki <harshithkalki@gmail.com>
2023-01-08 21:37:28 +0530

    added userform

--- src/pages/users/new.tsx
+++ src/pages/users/new.tsx
@@ -1,7 +1,37 @@
+import type { UserFormType } from "@/components/UserForm";
+import UserForm from "@/components/UserForm";
 import React from "react";
 
+const onSubmit = async (values: UserFormType, actions: any) => {
+  console.log(values);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
+
 const app = () => {
-  return <div>new</div>;
+  return (
+    <>
+      <UserForm
+        title="Create User"
+        // onSubmit={onSubmit}
+        formInputs={{
+          firstname: "",
+          lastname: "",
+          middlename: "",
+          phone: "",
+          addressline1: "",
+          addressline2: "",
+          city: "",
+          state: "",
+          country: "",
+          pincode: "",
+          role: "",
+          linkedto: "",
+          email: "",
+        }}
+      />
+    </>
+  );
 };
 
 export default app;
