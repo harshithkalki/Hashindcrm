d21da8f8993f29679eecc934e38e45cb8fc6d2c4
Abhiram <anukoolaabhiram@outlook.com>
2023-06-14 22:51:21 +0530

    round off added for cash,bank and upi

--- src/components/Cash,BankandUpi/CashTable.tsx
+++ src/components/Cash,BankandUpi/CashTable.tsx
@@ -32,6 +32,7 @@ const CashTable = () => {
                   date: dayjs(doc.date).format('DD MMMM YYYY'),
                   index: index + 10 * (page - 1) + 1,
                   customer: doc.customer?.name ?? 'N/A',
+                  total: Math.round(doc.total),
                 })) || []
             }
             colProps={{
